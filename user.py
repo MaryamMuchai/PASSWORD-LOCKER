61062b35e5ba40f6fba445fa3103e71acd763531
Marya <maryammuchai@gmail.com>
2021-04-11 13:16:57 +0300

    initializing the account object

--- user.py
+++ user.py
@@ -26,3 +26,13 @@ class Records:
     class that creates new account credentials
     '''
     account_records=[] #showing the list of all accounts
+
+    def__init__(self, account_name, user_name, password = None):
+    '''
+    initialization of the account object
+    '''
+    self.account_name = account_name
+    self.user_name = Username
+    self.password = password if password else Records.password_generate()
+
+
